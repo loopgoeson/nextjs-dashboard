db05021cb94526116467e51e2944f677a41f06cc
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2024-02-13 00:21:47 +0530

    error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,11 +24,16 @@ const {customerId,amount,status} = CreateInvoice.parse({
 const amountInCents =amount*100;
 const date=new Date().toISOString().split('T')[0];
 
+try{
 await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-
+}catch(error){
+  return {
+message: 'Database error: Failed to update Invoice.',
+};
+}
 revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 
@@ -42,17 +47,30 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+  } catch(error){
+  return {
+message: 'Database error: Failed to update Invoice.',
+};
+}
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+try {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+ return {
+message : 'Deleted Invoice.'
+}; 
+}
+catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
